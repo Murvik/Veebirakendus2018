fa85a025b803ffc43c1a1b4c21f761f467f7060e
Steven <steven.leego@gmail.com>
2018-02-25 16:06:06 +0200

    Kirjutan eelmise projekti üle, too ei buildinud.

--- src/test/java/veebirakendused/Veebirakendus2018/Veebirakendus2018ApplicationTests.java
+++ src/test/java/veebirakendused/Veebirakendus2018/Veebirakendus2018ApplicationTests.java
@@ -1,4 +1,4 @@
-package ee.ut.cs.wad.minuapp;
+package veebirakendused.Veebirakendus2018;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MinuAppApplicationTests {
+public class Veebirakendus2018ApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
