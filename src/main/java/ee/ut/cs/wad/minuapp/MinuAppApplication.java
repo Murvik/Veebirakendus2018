ced0e43c55fc8915d899b480d0cd066e23af2a7d
Steven <steven.leego@gmail.com>
2018-02-25 12:30:33 +0200

    Esialgne setup

---
+++ src/main/java/ee/ut/cs/wad/minuapp/MinuAppApplication.java
@@ -0,0 +1,12 @@
+package ee.ut.cs.wad.minuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinuAppApplication.class, args);
+    }
+}
